e791805f26228f5e0d46e4da2fa7ad9dc6650098
zelkovaria <hyein396@gmail.com>
2025-02-28 23:21:21 +0900

    [FE] feat: CategorySection에 VoiceChannelController 배치 (#74)

--- src/frontend/src/pages/FriendsPage/components/CategorySection/index.tsx
+++ src/frontend/src/pages/FriendsPage/components/CategorySection/index.tsx
@@ -3,6 +3,8 @@ import GuildCategory from '@/components/guild/GuildCategory';
 import UserProfile from '@/pages/FriendsPage/components/UserProfile';
 import { useGuildInfoStore } from '@/stores/guildInfo';
 
+import VoiceChannelController from '../VoiceChannelController';
+
 import * as S from './styles';
 
 const CategorySection = () => {
@@ -11,6 +13,7 @@ const CategorySection = () => {
   return (
     <S.CategorySectionContainer>
       <S.CategoryItemWrapper>{guildId ? <GuildCategory /> : <DirectMessageCategory />}</S.CategoryItemWrapper>
+      <VoiceChannelController />
       <UserProfile
         userName="Fe"
         userImageUrl=""
